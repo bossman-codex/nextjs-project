78f226edd02a080a11381e79353b67d40d78a03f
bossman-codex <okparathankgod@gmail.com>
2023-03-16 16:29:57 +0000

    project completed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+       // pathname: '/account123/**',
+      },
+    ],
+  },
 }
 
-module.exports = nextConfig
